a5039688da6c849c50d779dcd95792c649873331
marcdumon <marcdumon@msn.com>
2020-05-08 13:03:12 +0200

    Added plotly_discrete_colorscale

---
+++ my_tools/plotly_discrete_colorscale.py
@@ -0,0 +1,24 @@
+# --------------------------------------------------------------------------------------------------------
+# 2020/05/08
+# my_tools - plotly_discrete_colorscale.py
+# md
+# --------------------------------------------------------------------------------------------------------
+
+
+# Source: https://chart-studio.plotly.com/~empet/15229.embed
+
+def plotly_discrete_colorscale(bvals, colors):
+    """
+    bvals - list of values bounding intervals/ranges of interest
+    colors - list of rgb or hex colorcodes for values in [bvals[k], bvals[k+1]],0<=k < len(bvals)-1
+    returns the plotly  discrete colorscale
+    """
+    if len(bvals) != len(colors) + 1:
+        raise ValueError('len(boundary values) should be equal to  len(colors)+1')
+    bvals = sorted(bvals)
+    nvals = [(v - bvals[0]) / (bvals[-1] - bvals[0]) for v in bvals]  # normalized values
+
+    dcolorscale = []  # discrete colorscale
+    for k in range(len(colors)):
+        dcolorscale.extend([[nvals[k], colors[k]], [nvals[k + 1], colors[k]]])
+    return dcolorscale
